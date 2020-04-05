1d3919178ec5f06f3ff17547d88d278defaac36e
aferrero2707 <aferrero1975@gmail.com>
2020-04-05 14:03:10 +0200

    Removed apt-get calls from check_commit.sh script

--- check_commit.sh
+++ check_commit.sh
@@ -9,8 +9,8 @@ echo "########################################################################"
 echo ""
 echo "Checking commit hash"
 echo ""
-sudo apt-get -y update
-sudo apt-get install -y wget git || exit 1
+#sudo apt-get -y update
+#sudo apt-get install -y wget git || exit 1
 rm -f /tmp/commit-${GIT_BRANCH}.hash
 wget https://github.com/${TRAVIS_REPO_SLUG}/releases/download/${RELEASE}/commit-${GIT_BRANCH}-${OS_NAME}.hash -O /tmp/commit-${GIT_BRANCH}.hash
 
