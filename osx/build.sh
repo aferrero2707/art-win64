cc5e9d5e9a73b1fd936547fb8b5454e4cb84dea3
aferrero2707 <aferrero1975@gmail.com>
2020-04-05 14:25:29 +0200

    Update build.sh

--- osx/build.sh
+++ osx/build.sh
@@ -6,7 +6,7 @@ export LD_LIBRARY_PATH=$MPPREFIX/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$MPPREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
 
 if [ x"${TRAVIS_OS_NAME}" == "xosx" ]; then
-(wget https://github.com/aferrero2707/macports-travis-cache/releases/download/continuous/macports-travis-cache-7.3-2.tgz && sudo tar xf macports-travis-cache-7.3-2.tgz) || exit 1
+(wget -q https://github.com/aferrero2707/macports-travis-cache/releases/download/continuous/macports-travis-cache-7.3-2.tgz && sudo tar xf macports-travis-cache-7.3-2.tgz) || exit 1
 port install exiv2 lcms2 lensfun fftw-3 gtk-osx-application-gtk3 || exit 1
 fi
 
