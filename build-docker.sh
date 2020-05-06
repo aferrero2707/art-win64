0d30e68a39e56f96f858e867284e9786c87300bc
aferrero2707 <aferrero1975@gmail.com>
2020-05-06 19:59:20 +0200

    updater docker test script

--- build-docker.sh
+++ build-docker.sh
@@ -6,7 +6,7 @@ if [ ! -e RawTherapee ]; then
 	git clone https://bitbucket.org/agriggio/art.git --branch $RT_BRANCH --single-branch RawTherapee
 fi
 rm -rf RawTherapee/ci
-cp -a ci RawTherapee
+cp -a win64 RawTherapee/ci
 cd RawTherapee
 #docker run -it -v $(pwd):/sources -e "RT_BRANCH=$RT_BRANCH" photoflow/docker-centos7-gtk bash 
 #/sources/ci/appimage-centos7.sh
